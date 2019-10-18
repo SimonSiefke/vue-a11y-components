de7b668912905aac04e323cf266dcac5b2b803c9
Simon Siefke <simon.siefke@gmail.com>
2019-10-18 13:11:49 +0200

    fix: prevent default on button events

--- src/components/ListBox/ListBox.vue
+++ src/components/ListBox/ListBox.vue
@@ -11,58 +11,59 @@ export default {
     return {
       visible: false,
       buttonId: `button-${`${Math.random()}`.slice(2, 10)}`,
-      listId: `list-${`${Math.random()}`.slice(2, 10)}`,
-    }
+      listId: `list-${`${Math.random()}`.slice(2, 10)}`
+    };
   },
   provide() {
     return {
       listBox: {
         listId: this.listId,
-        buttonId: this.buttonId,
-      },
-    }
+        buttonId: this.buttonId
+      }
+    };
   },
   mounted() {
-    const $listBoxButton = document.getElementById(this.buttonId)
-    const $listBoxList = document.getElementById(this.listId)
+    const $listBoxButton = document.getElementById(this.buttonId);
+    const $listBoxList = document.getElementById(this.listId);
     const show = () => {
-      $listBoxList.style.display = 'block'
-      $listBoxButton.setAttribute('aria-expanded', 'true')
-      $listBoxList.focus()
-    }
+      $listBoxList.style.display = "block";
+      $listBoxButton.setAttribute("aria-expanded", "true");
+      $listBoxList.focus();
+    };
     const hide = () => {
-      $listBoxButton.removeAttribute('aria-expanded')
-      $listBoxList.style.display = 'none'
-      $listBoxButton.focus()
-    }
-    const toggleVisible = () => {
-      if ($listBoxList.style.display === 'block') {
-        hide()
+      $listBoxButton.removeAttribute("aria-expanded");
+      $listBoxList.style.display = "none";
+      $listBoxButton.focus();
+    };
+    const toggleVisible = event => {
+      event.preventDefault();
+      if ($listBoxList.style.display === "block") {
+        hide();
       } else {
-        show()
+        show();
       }
-    }
+    };
     const hideOnBlur = event => {
       if (!$listBoxButton.contains(event.relatedTarget)) {
-        hide()
+        hide();
       }
-    }
-    $listBoxButton.addEventListener('click', toggleVisible)
-    $listBoxList.addEventListener('blur', hideOnBlur)
+    };
+    $listBoxButton.addEventListener("click", toggleVisible);
+    $listBoxList.addEventListener("blur", hideOnBlur);
     const hideOnEscape = event => {
-      if (['Escape', 'Enter', 'Return'].includes(event.key)) {
-        event.preventDefault()
-        hide()
+      if (["Escape", "Enter", "Return"].includes(event.key)) {
+        event.preventDefault();
+        hide();
       }
-    }
-    $listBoxList.addEventListener('keydown', hideOnEscape)
-    $listBoxList.addEventListener('click', hide)
-    this.$once('hook:beforeDestroy', () => {
-      $listBoxButton.removeEventListener('click', toggleVisible)
-      $listBoxList.removeEventListener('blur', hideOnBlur)
-      $listBoxList.removeEventListener('keydown', hideOnEscape)
-      $listBoxList.removeEventListener('click', hide)
-    })
-  },
-}
+    };
+    $listBoxList.addEventListener("keydown", hideOnEscape);
+    $listBoxList.addEventListener("click", hide);
+    this.$once("hook:beforeDestroy", () => {
+      $listBoxButton.removeEventListener("click", toggleVisible);
+      $listBoxList.removeEventListener("blur", hideOnBlur);
+      $listBoxList.removeEventListener("keydown", hideOnEscape);
+      $listBoxList.removeEventListener("click", hide);
+    });
+  }
+};
 </script>
